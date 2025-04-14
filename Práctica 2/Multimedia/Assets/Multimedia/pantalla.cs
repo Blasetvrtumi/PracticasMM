2f466d92031d21deab8d0d70b4c4859a702e6693
Blas Vita Ramos <117302429+Blasetvrtumi@users.noreply.github.com>
2025-04-14 21:39:36 +0200

    ImageTarget Added

---
+++ Práctica 2/Multimedia/Assets/Multimedia/pantalla.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class pantalla : MonoBehaviour {
+
+public enum TrackingStatusFilter
+{
+Tracked,
+Tracked_ExtendedTracked,
+Tracked_ExtendedTracked_Limited
+}
+
+public VideoPlayer pantallaVideo;
+}
+
